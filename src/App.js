dd1df3685ebd648820bf87416e93e76095d81baa
User <user@MacBook-Pro-3.local>
2021-07-21 12:03:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import CreateNft from "./components/CreateNft";
+import Auth from "./Auth";
+import { useMoralis } from "react-moralis";
+import { BrowserRouter, Switch, Route, Redirect, Link } from "react-router-dom";
+import {
+  Container,
+  Heading,
+  Avatar,
+  Flex,
+  Spacer,
+  Button,
+  Center
+} from "@chakra-ui/react";
+import { AddIcon, SearchIcon } from "@chakra-ui/icons";
 
 function App() {
+  const {
+    authenticate,
+    isAuthenticated,
+    isAuthUndefined,
+    isAuthenticating,
+    user,
+    logout,
+    authError
+  } = useMoralis();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Container maxW="container.xl">
+          <Flex my={6}>
+            <Link to="/">
+              {" "}
+              <Heading>Home</Heading>
+            </Link>
+            <Spacer />
+            {isAuthenticated && (
+              <Link to="/profile">
+                <Avatar name={user.attributes.username} mr="4"></Avatar>
+              </Link>
+            )}
+
+            <Link to="/create-nft">
+              {" "}
+              <Button leftIcon={<AddIcon />} mr="4" colorScheme="green">
+                Create
+              </Button>
+            </Link>
+            <Button onClick={() => logout()}>Disconnect</Button>
+          </Flex>
+        </Container>
+        <Container maxW="container.xl">
+          <Center>
+            <Heading mb={6}>Welcome to my nft marketplace</Heading>
+          </Center>
+
+          {isAuthenticated ? (
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+
+              <Route path="/profile" exact>
+                <Profile />
+              </Route>
+
+              <Route path="/create-nft" exact>
+                <CreateNft />
+              </Route>
+            </Switch>
+          ) : (
+            <>
+              {!isAuthUndefined && <Redirect to="/" />}
+              <Auth />{" "}
+            </>
+          )}
+        </Container>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { MoralisProvider } from "react-moralis";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import tokenContractAbi from "./abi";
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark"
+  }
+});
+
+const appId = "Xqf7tAVNAPWWJnHngpHxM2E86dGum976krTasEZq";
+const serverUrl = "https://dsn5dwkdmj48.usemoralis.com:2053/server";
+const TOKEN_CONTRACT_ADDRESS = "0x8BC3BF53e6059f489897f0A859CF6D102fe7daa4";
+// window.web3 = Moralis.Web3.enable();
+// window.tokenContract = new web3.eth.Contract(
+//   tokenContractAbi,
+//   TOKEN_CONTRACT_ADDRESS
+// );
+
+// const mintNft = async metadataUrl => {
+//   const receipt = await tokenContract.methods.createItem;
+// };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
